40085a4c5717f6dad832da43901b1a212b0d1897
unknown <balvalloire@gmail.com>
2021-05-21 21:02:48 +0200

    :sparkles: animation

--- src/scripts/Animation.js
+++ src/scripts/Animation.js
@@ -8,10 +8,52 @@ export default class Animation {
 
     initEls() {
         this.cube = document.querySelector('.js-cube');
-        this.line = document.querySelector('.js-line');
+        this.container = document.querySelector('.js-container');
     }
 
     loopAnimation() {
+        const tl = new gsap.timeline({
+            repeat: -1
+        });
 
+        tl.to(this.cube, {
+            rotate: -90,
+            duration: 0.5,
+            delay: 0.2,
+            transformOrigin: '0% 100%',
+            ease: 'power4.inOut'
+        })
+            .set(this.cube, {
+                x: -50,
+                rotate: 0
+            })
+            .to(this.cube, {
+                rotate: -270,
+                duration: 1,
+                delay: 0.2,
+                y: -25,
+                transformOrigin: '0% 100%',
+                ease: 'power4.inOut'
+            })
+            .to(this.container, {
+                rotate: 180,
+                duration: 1,
+                ease: 'back.inOut'
+            }, '-=1')
+            .set(this.container, {
+                rotate: 0
+            })
+            .set(this.cube, {
+                x: 50,
+                y: -26.5,
+                rotate: 0
+            })
+            .to(this.cube, {
+                rotate: -90,
+                duration: 0.5,
+                delay: 0.2,
+                transformOrigin: '0% 100%',
+                ease: 'power4.inOut'
+            });
     }
 }
